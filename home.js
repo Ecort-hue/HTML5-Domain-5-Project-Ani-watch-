aad2e06c32402f51ccc1579ed019ad51b893e43b
Ecort-hue <joshuacortes017@gmail.com>
2026-01-07 12:06:18 -0500

    Commit 2
    Instead of displaying each anime one by one, I decide it was a good idea to use a for loop. Now i can display 3 without repeating the same line of code each time. Also preparation to include Top Anime in this.

--- home.js
+++ home.js
@@ -5,19 +5,27 @@ ani_list.onreadystatechange = function () {
        data = JSON.parse(this.responseText);
        console.log(data);
 
-        const a_img = document.querySelector('.a_img');
-        a_img.src = data.data[2].images.jpg.image_url;
-    const a_title = document.querySelector('.a_title');
-    a_title.innerHTML = data.data[2].title;
     
-
-
-       
-        
+for (let i = 0; i < 3; i++) {
+    const upcoming_list = document.querySelector('.upcoming-list');
+    const list_a = document.createElement('div');
+    list_a.className = 'list_a';
+    list_a.innerHTML = `<img class="list_img" src="${data.data[i].images.jpg.image_url}">
+        <div class="list_info">
+        <h3 class="list_title">${data.data[i].title}</h3>
+        </div>`;
+    upcoming_list.appendChild(list_a);
+}   
     }}
 ;
 
-ani_list.open("GET", `https://api.jikan.moe/v4/seasons/upcoming`, true);
+
+
+
+
+
+
+ani_list.open("GET", `https://api.jikan.moe/v4/seasons/upcoming`, 'https://api.jikan.moe/v4/top/anime?sfw',  true);
 ani_list.send();
 
 
