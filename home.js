4f147cf19ca8a866432201d62529ecbc4b972650
Ecort-hue <joshuacortes017@gmail.com>
2026-01-09 12:18:04 -0500

    Ive started to work on my other section of the website, using almost the same line of code but with a different set of json data.
    Theres a error on line 55, will look into fixing it.

--- home.js
+++ home.js
@@ -1,9 +1,10 @@
-var ani_list = new XMLHttpRequest();
+var upcoming = new XMLHttpRequest();
+
 let data;
-ani_list.onreadystatechange = function () {
+upcoming.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
        data = JSON.parse(this.responseText);
-       console.log(data);
+        console.log(data);
 
     
 for (let i = 0; i < 3; i++) {
@@ -15,9 +16,31 @@ for (let i = 0; i < 3; i++) {
         <h3 class="list_title">${data.data[i].title}</h3>
         </div>`;
     upcoming_list.appendChild(list_a);
+
+    var top = new XMLHttpRequest();
+
+    top.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            data = JSON.parse(this.responseText);
+            console.log(data);
+
+
+            for (let j = 0; j < 3; j++) {
+                const top_list = document.querySelector('.top-list');
+                const list_b = document.createElement('div');
+                list_b.className = 'list_b';
+                list_b.innerHTML = `<img class="list_img" src="${data.data[j].images.jpg.image_url}">
+        <div class="list_info">
+        <h3 class="list_title">${data.data[j].title}</h3>
+        </div>`;
+                top_list.appendChild(list_b);
+            }
+        }
+    };
+
+    
 }   
-    }}
-;
+    }};
 
 
 
@@ -25,7 +48,8 @@ for (let i = 0; i < 3; i++) {
 
 
 
-ani_list.open("GET", `https://api.jikan.moe/v4/seasons/upcoming`, 'https://api.jikan.moe/v4/top/anime?sfw',  true);
-ani_list.send();
 
-
+upcoming.open("GET", `https://api.jikan.moe/v4/seasons/upcoming`,  true);
+upcoming.send();
+top.open("GET", `top.json`, true);
+top.send();
