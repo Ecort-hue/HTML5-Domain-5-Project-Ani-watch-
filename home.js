e2b0c20cf35ac9d31ef8b7261aa4e0dccbab6187
Ecort-hue <joshuacortes017@gmail.com>
2026-01-12 12:11:28 -0500

    Reworked Lines 6-20
    When trying to get another request 'status' appeared deprecated, this rework should allow for another request to work along side with 'upcoming' anime's

--- home.js
+++ home.js
@@ -1,46 +1,40 @@
 var upcoming = new XMLHttpRequest();
-
+var top = new XMLHttpRequest();
 let data;
+
+
 upcoming.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
        data = JSON.parse(this.responseText);
+       data = data.data;
         console.log(data);
 
-    
-for (let i = 0; i < 3; i++) {
-    const upcoming_list = document.querySelector('.upcoming-list');
-    const list_a = document.createElement('div');
-    list_a.className = 'list_a';
-    list_a.innerHTML = `<img class="list_img" src="${data.data[i].images.jpg.image_url}">
+
+        for (let i = 0; i < 3; i++) {
+            const upcoming_list = document.querySelector('.upcoming-list');
+            const list_a = document.createElement('div');
+            list_a.className = 'list_a';
+            list_a.innerHTML = `<img class="list_img" src="${data[i].images.jpg.image_url}">
         <div class="list_info">
-        <h3 class="list_title">${data.data[i].title}</h3>
+        <h3 class="list_title">${data[i].title}</h3>
         </div>`;
-    upcoming_list.appendChild(list_a);
-
-    var top = new XMLHttpRequest();
-
-    top.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            data = JSON.parse(this.responseText);
-            console.log(data);
-
-
-            for (let j = 0; j < 3; j++) {
-                const top_list = document.querySelector('.top-list');
-                const list_b = document.createElement('div');
-                list_b.className = 'list_b';
-                list_b.innerHTML = `<img class="list_img" src="${data.data[j].images.jpg.image_url}">
-        <div class="list_info">
-        <h3 class="list_title">${data.data[j].title}</h3>
-        </div>`;
-                top_list.appendChild(list_b);
-            }
+            upcoming_list.appendChild(list_a);
         }
-    };
+    }
 
+
+}
     
-}   
-    }};
+
+
+// let top_data;
+//   top.onreadystatechange = function() {
+//     if (top.readyState === 4 && top.status === 200) {
+//         console.log(top.responseText);
+
+//     }
+//   };
+    
 
 
 
@@ -49,7 +43,9 @@ for (let i = 0; i < 3; i++) {
 
 
 
-upcoming.open("GET", `https://api.jikan.moe/v4/seasons/upcoming`,  true);
+
+
+upcoming.open("GET", `https://api.jikan.moe/v4/seasons/upcoming`, true);
 upcoming.send();
-top.open("GET", `top.json`, true);
-top.send();
+// top.open("GET", `https://api.jikan.moe/v4/top/anime?type=ona`, true);
+// top.send();
